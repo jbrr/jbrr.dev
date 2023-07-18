1cfe58f6cdf24306f26ce9d8b6fdd379f8c29f96
Jeff Ruane <ruane.jb@gmail.com>
2023-07-18 05:00:38 -0600

    rename

--- .tina/config.ts
+++ .tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "13c3bac8-2c90-45eb-95cf-f0b06c094f33", // Get this from tina.io
-  token: "4f36528db63e5b996fdfcb3b30c4cdd3eec7a153", // Get this from tina.io
+  clientId: "13c3bac8-2c90-45eb-95cf-f0b06c094f33", // Get this from .tina.io
+  token: "4f36528db63e5b996fdfcb3b30c4cdd3eec7a153", // Get this from .tina.io
 
   build: {
     outputFolder: "admin",
