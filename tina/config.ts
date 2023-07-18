a7f876cc885785950734d749556ad47260e63065
Jeff Ruane <ruane.jb@gmail.com>
2023-07-18 11:31:47 -0600

    fix schema

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,61 @@
-import { defineConfig } from "tinacms";
+import { defineConfig, TinaField } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+const blogSchema = [
+  {
+    type: "string",
+    name: "title",
+    label: "Title",
+    isTitle: true,
+    required: true,
+  },
+  {
+    type: "string",
+    name: "author",
+    label: "Author",
+    required: false
+  },
+  {
+    type: "datetime",
+    name: "pubDatetime",
+    label: "Published Date",
+    required: true
+  },
+  {
+    type: "rich-text",
+    name: "body",
+    label: "Body",
+    isBody: true,
+  },
+  {
+    type: "string",
+    name: "postSlug",
+    label: "Post Slug",
+  },
+  {
+    type: "boolean",
+    name: "featured",
+    label: "Featured",
+  },
+  {
+    type: "boolean",
+    name: "draft",
+    label: "Draft",
+  },
+  {
+    type: "string",
+    name: "tags",
+    label: "Tags",
+    list: true,
+  },
+  {
+    type: "string",
+    name: "ogImage",
+    label: "Post Image"
+  },
+] as TinaField<false>[];
 
 export default defineConfig({
   branch,
@@ -21,62 +75,18 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        name: "post",
+        name: "posts",
         label: "Posts",
         path: "src/content/blog",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "string",
-            name: "author",
-            label: "Author",
-            required: false
-          },
-          {
-            type: "datetime",
-            name: "pubDatetime",
-            label: "Published Date",
-            required: true
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-          {
-            type: "string",
-            name: "postSlug",
-            label: "Post Slug",
-          },
-          {
-            type: "boolean",
-            name: "featured",
-            label: "Featured",
-          },
-          {
-            type: "boolean",
-            name: "draft",
-            label: "Draft",
-          },
-          {
-            type: "string",
-            name: "tags",
-            label: "Tags",
-            list: true,
-          },
-          {
-            type: "string",
-            name: "ogImage",
-            label: "Post Image"
-          },
-        ],
+        format: "mdx",
+        fields: blogSchema,
+      },
+      {
+        name: "posts-md",
+        label: "Posts (Markdown)",
+        path: "src/content/blog",
+        format: "md",
+        fields: blogSchema,
       },
     ],
   },
