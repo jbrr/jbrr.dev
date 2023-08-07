e9ebe25b4012e6795549495d1dfa17f1adf41988
Jeff Ruane <ruane.jb@gmail.com>
2023-08-07 13:58:41 -0600

    add some microformats

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,17 +17,21 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          <h2 className="h-name text-lg font-medium decoration-dashed hover:underline">
             {title}
           </h2>
         ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
+          <h3 className="h-name text-lg font-medium decoration-dashed hover:underline">
             {title}
           </h3>
         )}
       </a>
-      <Datetime datetime={pubDatetime} includeTime={pubDatetimeIncludeTime} />
-      <p>{description}</p>
+      <Datetime
+        datetime={pubDatetime}
+        includeTime={pubDatetimeIncludeTime}
+        className="dt-published"
+      />
+      <p className="p-summary">{description}</p>
     </li>
   );
 }
